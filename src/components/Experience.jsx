69feb281335218fd2941c6ea70f3f396d14c6792
Andrey <atasylle@gmail.com>
2023-11-04 15:29:20 +0100

    minor changes

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -8,6 +8,7 @@ import nextjs from "../assets/nextjs.png";
 import graphql from "../assets/graphql.png";
 import github from "../assets/github.png";
 import tailwind from "../assets/tailwind.png";
+import vue from "../assets/vue.png";
 
 const Experience = () => {
   const techs = [
@@ -59,6 +60,12 @@ const Experience = () => {
       title: "GitHub",
       style: "shadow-gray-400",
     },
+    {
+      id: 9,
+      src: vue,
+      title: "Vue",
+      style: "shadow-green-400",
+    },
   ];
 
   return (
@@ -71,17 +78,17 @@ const Experience = () => {
           <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
             Experience
           </p>
-          <p className="py-6">These are the technologies I've worked with</p>
+          <p className="py-4">These are the technologies I've worked with</p>
         </div>
 
-        <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
+        <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-4 text-center py-4 px-6 sm:px-0">
           {techs.map(({ id, src, title, style }) => (
             <div
               key={id}
               className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
             >
-              <img src={src} alt="" className="w-20 mx-auto" />
-              <p className="mt-4">{title}</p>
+              <img src={src} alt="" className="w-16 mx-auto" />
+              <p className="mt-2">{title}</p>
             </div>
           ))}
         </div>
